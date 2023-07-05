947291726e10d2a3906ba5d74054fb65f55f5d47
Juliano Gregorio <julianosgreg@gmail.com>
2023-07-04 22:54:11 -0300

    feat: add deleteOpening handler logic

--- handler/response.go
+++ handler/response.go
@@ -18,7 +18,7 @@ func sendError(ctx *gin.Context, code int, msg string) {
 func sendSuccess(ctx *gin.Context, op string, data interface{}) {
 	ctx.Header("Content-type", "application/json")
 	ctx.JSON(http.StatusOK, gin.H{
-		"message": fmt.Sprintf("operation from handler%s successfull", op),
+		"message": fmt.Sprintf("operation from handler %s successfull", op),
 		"data":    data,
 	})
 }
